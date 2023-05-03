8049180c64b5f6cfae24e0bf4df0d9bb3161ccf5
CipherTD <tien03dat@gmail.com>
2023-05-03 20:21:31 +0700

    update service, checkout, dataconector

--- src/main/java/app/ie303hotelmanagement/HotelManagement.java
+++ src/main/java/app/ie303hotelmanagement/HotelManagement.java
@@ -10,12 +10,7 @@ import java.io.IOException;
 public class HotelManagement extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-        FXMLLoader fxmlLoader = new FXMLLoader(HotelManagement.class.getResource("Service.fxml"));
-=======
         FXMLLoader fxmlLoader = new FXMLLoader(HotelManagement.class.getResource("Login-Page.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(HotelManagement.class.getResource("Check-out.fxml"));
->>>>>>> eada7baa28d1f4df4f2d6a848a6c67ce4ef83927
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Service");
         stage.setScene(scene);
